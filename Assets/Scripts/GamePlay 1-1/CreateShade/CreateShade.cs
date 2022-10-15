1ee59a8df8d8dba0b3e1f281b7b971eda0768307
Matteotti <mr.matteotti@outlook.com>
2022-10-15 16:11:05 +0800

    all done except for dialogue text & player anim & merge

---
+++ Assets/Scripts/GamePlay 1-1/CreateShade/CreateShade.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateShade : MonoBehaviour
+{
+    public List<GameObject> nutList = new List<GameObject>();
+    public AudioPlayerManager audioManager;
+    void Update()
+    {
+        if(nutList.Count > 0 && Input.GetMouseButtonDown(0))
+        {
+            audioManager.Wind();
+            Invoke(nameof(StopPlayingAudio), 1);
+            nutList[0].transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            nutList[0].AddComponent<Rigidbody2D>();
+            nutList[0].GetComponent<Rigidbody2D>().gravityScale = 1;
+            nutList[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            nutList.Remove(nutList[0]);
+        }
+    }
+    void StopPlayingAudio()
+    {
+        if (audioManager.effect.clip != null)
+            audioManager.effect.Stop();
+    }
+}
--- Assets/Scripts/GamePlay 1-1/Trap/Shade.cs
+++ Assets/Scripts/GamePlay 1-1/Trap/Shade.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class Shade : MonoBehaviour
 {
     public GameObject boss;
-    private void OnTriggerEnter2D(Collider2D collision)
+    public bool startCheckGround;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            Destroy(GetComponent<Rigidbody2D>());
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
